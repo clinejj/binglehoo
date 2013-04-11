b6a172de7c725d340c631fa53a42d522fff8dfaf
John Cline <clinejj@gmail.com>
2013-04-11 16:31:50 -0400

    json, yahoo add

--- binglehoo/src/com/pixeltron/binglehoo/BinglehooServlet.java
+++ binglehoo/src/com/pixeltron/binglehoo/BinglehooServlet.java
@@ -17,40 +17,31 @@ import org.jsoup.select.Elements;
 
 @SuppressWarnings("serial")
 public class BinglehooServlet extends HttpServlet {
-	public static final String G_URL = "https://www.google.com/uds/GwebSearch?callback=google.search.WebSearch.RawCompletion&rsz=small&hl=en&source=gsc&gss=.com&sig=dafe20cc2afc0dcfa10b802f251c72d0&&q=&gl=www.google.com&oq=bing&gs_l=partner.12...0.0.2.14400.0.0.0.0.0.0.0.0..0.0.gsnos%2Cn%3D13..0.0.0..1ac.&qid=13dfa707f45f9643&context=1&key=notsupplied&v=1.0&nocache=1365706268260";
-	
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		resp.setContentType("text/html");
-
-		String qry = "cupcakes";
-		String gqry = G_URL;
-		gqry = gqry.replace("&q=", "&q=" + URLEncoder.encode(qry, "UTF-8"));
-		String gresp = executePost(gqry,"");
-    	if (gresp != null) {
-    		String ans = gresp.substring(gresp.indexOf("{"), gresp.lastIndexOf("}"));
-    		
-    		resp.getWriter().println(ans);
-    	}
-
-    	/*
-    	Document doc = Jsoup.parse(src);
-    	Elements newsHeadlines = doc.select("#rso");
-    	resp.getWriter().println(newsHeadlines.toString());
-    	
-    	*/
-	}
+	private static final String G_URL = "https://www.google.com/uds/GwebSearch?callback=google.search.WebSearch.RawCompletion&rsz=small&hl=en&source=gsc&gss=.com&sig=dafe20cc2afc0dcfa10b802f251c72d0&&q=&gl=www.google.com&oq=bing&gs_l=partner.12...0.0.2.14400.0.0.0.0.0.0.0.0..0.0.gsnos%2Cn%3D13..0.0.0..1ac.&qid=13dfa707f45f9643&context=1&key=notsupplied&v=1.0&nocache=1365706268260";
+	private static final String Y_URL = "http://search.yahoo.com/search;_ylt=Ag2rqzK7vv6CsmRCVSecZ16bvZx4?p=&toggle=1&cop=mss&ei=UTF-8&fr=yfp-t-900";
+	private static final String bingAcctKey = "wNsZUMQ6/Kws4zgDAgTSPR9c80hZkvCVFihcs+KNRvg=";
+	private static final String bingRootUri = "https://api.datamarket.azure.com/Bing/Search";
 	
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
 		throws IOException {
-    	String qry = req.getParameter(Query.QUERY_NAME);
+    	String qry = URLEncoder.encode(req.getParameter(Query.QUERY_NAME), "UTF-8");
     	String gqry = G_URL;
-		gqry = gqry.replace("&q=", "&q=" + URLEncoder.encode(qry, "UTF-8"));
+		gqry = gqry.replace("&q=", "&q=" + qry);
+		String yqry = Y_URL;
+		yqry = yqry.replace("?p=", "?p=" + qry);
+		
+		
+		
 		String gresp = executePost(gqry,"");
     	if (gresp != null) {
     		gresp = gresp.substring(gresp.indexOf("{"), gresp.lastIndexOf("}"));
     		resp.getWriter().println(gresp);
     	}
+    	
+    	Document doc = Jsoup.connect(yqry).get();
+    	Elements newsHeadlines = doc.select("#web");
+    	resp.getWriter().println(newsHeadlines.toString());
+
     }
     
     public static String executePost(String targetURL, String urlParameters)
--- binglehoo/src/com/pixeltron/binglehoo/Query.java
+++ binglehoo/src/com/pixeltron/binglehoo/Query.java
@@ -2,4 +2,9 @@ package com.pixeltron.binglehoo;
 
 public class Query {
 	public static final String QUERY_NAME = "query";
+	
+	public String url;
+	public String visibleUrl;
+	public String title;
+	public String content;
 }
